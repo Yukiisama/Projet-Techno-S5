7dcbebe7fb8335e2e85ea0e4a888c84cce009bb9
Alexis Perignon <aperigno@emi.u-bordeaux1.fr>
2018-12-09 13:48:28 +0100

    init

--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /net/cremi/aperigno/PROJET_TECHNO_L3/ptl3iug/tests
-# Build directory: /net/cremi/aperigno/PROJET_TECHNO_L3/ptl3iug/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test("create_window" "/net/cremi/aperigno/PROJET_TECHNO_L3/ptl3iug/build/tests/unit_tests" "create_window" "--durations" "yes" "--warn" "NoAssertions" "--name" "unit_tests ( build)")
-set_tests_properties("create_window" PROPERTIES  FAIL_REGULAR_EXPRESSION "No tests ran" LABELS ";unit;" TIMEOUT "10")
